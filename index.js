534f4fdb3a8c9332095ac50a2cebdb40107b523e
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-17 13:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your Circle class here
+const pi = Math.PI
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+    return this.radius * 2
+  }
+
+  get circumference(diameter) {
+    return pi * diameter;
+  }
+  //
+  // get area() {
+  //   return `pi * ${this.radius} * 2`;
+  // }
+
+  set diameter(diameter) {
+    return this.radius * 2
+  }
+  //
+  set circumference(diameter) {
+    return pi * diameter;
+  }
+  //
+  // set area() {
+  //   return `pi * ${this.radius} * 2`;
+  // }
+}
